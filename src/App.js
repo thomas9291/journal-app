261f9f1d90daea96301bac094dc1312760258988
thomas jubin <thomas.jubin92@gmail.com>
2023-05-26 09:34:09 +0200

    step 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import classes from "./App.module.css";
 
 function App() {
-  return <div className={classes.app}></div>;
+  return (
+    <div className={classes.container}>
+      <h1>journal app</h1>
+    </div>
+  );
 }
 
 export default App;
